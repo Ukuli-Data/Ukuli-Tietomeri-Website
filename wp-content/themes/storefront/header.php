03c590935c22134377649ff4a515d6eac579b769
Tomi Toivio <tomi.toivio@ukuli.fi>
2020-08-11 13:41:42 +0300

    WordPress

---
+++ wp-content/themes/storefront/header.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package storefront
+ */
+
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-149120320-9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-149120320-9');
+</script>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '2548235701912385',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v8.0'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php wp_body_open(); ?>
+
+<?php do_action( 'storefront_before_site' ); ?>
+
+<div id="page" class="hfeed site">
+	<?php do_action( 'storefront_before_header' ); ?>
+
+	<header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
+
+		<?php
+		/**
+		 * Functions hooked into storefront_header action
+		 *
+		 * @hooked storefront_header_container                 - 0
+		 * @hooked storefront_skip_links                       - 5
+		 * @hooked storefront_social_icons                     - 10
+		 * @hooked storefront_site_branding                    - 20
+		 * @hooked storefront_secondary_navigation             - 30
+		 * @hooked storefront_product_search                   - 40
+		 * @hooked storefront_header_container_close           - 41
+		 * @hooked storefront_primary_navigation_wrapper       - 42
+		 * @hooked storefront_primary_navigation               - 50
+		 * @hooked storefront_header_cart                      - 60
+		 * @hooked storefront_primary_navigation_wrapper_close - 68
+		 */
+		do_action( 'storefront_header' );
+		?>
+
+	</header><!-- #masthead -->
+
+	<?php
+	/**
+	 * Functions hooked in to storefront_before_content
+	 *
+	 * @hooked storefront_header_widget_region - 10
+	 * @hooked woocommerce_breadcrumb - 10
+	 */
+	do_action( 'storefront_before_content' );
+	?>
+
+	<div id="content" class="site-content" tabindex="-1">
+		<div class="col-full">
+
+		<?php
+		do_action( 'storefront_content_top' );
